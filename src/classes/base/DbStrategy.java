792f2a042e12a7889def2e460d7376d12c7b9fd5
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-11-12 19:12:34 +0200

    ..

--- src/classes/base/DbStrategy.java
+++ src/classes/base/DbStrategy.java
@@ -2,8 +2,10 @@ package src.classes.base;
 
 public interface DbStrategy {
 
-    public boolean add(Table table);
-    public boolean remove(Table table);
+    public boolean add(Table table) throws Exception;
+    public boolean remove(Table table) throws Exception;
     public Table get(String name);
+    
+    public boolean contains(Repo<?> repo);
 
 }
--- src/classes/base/ListStrategy.java
+++ src/classes/base/ListStrategy.java
@@ -1,19 +1,59 @@
 package src.classes.base;
 
+import src.classes.repos.ListRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListStrategy implements DbStrategy{
 
+    List<ListRepo<?>> list;
+
     @Override
-    public boolean add(Table table) {
-        return false;
+    public boolean add(Table table) throws Exception{
+        Class<?> clazz= table.getClass();
+
+        ListRepo<?> repo = ListRepo.newRepo(clazz);
+
+        int repoIndex;
+
+        if(!contains(repo))
+            list.add(repo);
+
+        repoIndex = list.indexOf(repo);
+
+
+        return list.get(repoIndex).add(table);
+
     }
 
     @Override
-    public boolean remove(Table table) {
-        return false;
+    public boolean remove(Table table) throws Exception{
+        Class<?> clazz= table.getClass();
+
+        ListRepo<?> repo = ListRepo.newRepo(clazz);
+
+        int repoIndex;
+
+        if(!contains(repo))
+            list.add(repo);
+
+        repoIndex = list.indexOf(repo);
+
+        return list.get(repoIndex).remove(table);
     }
 
     @Override
     public Table get(String name) {
         return null;
     }
+
+    @Override
+    public boolean contains(Repo<?> repo){
+        return list.contains(repo);
+    }
+    public ListStrategy(){
+        list=new ArrayList<>();
+    }
+
 }
