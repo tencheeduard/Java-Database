8ecdaf25db87a3cafb0a1070716917a55c8ebc2f
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-11-12 20:31:49 +0200

    . ..

--- src/classes/base/CLIController.java
+++ src/classes/base/CLIController.java
@@ -7,12 +7,9 @@ public class CLIController {
     Database base;
     public void start(){
         System.out.println("Type of db:");
-        System.out.println("1.List");
-        System.out.println("2.CvCute<3");
-        Scanner scanner = new Scanner(System.in);
-        int choice= scanner.nextInt();
-        if (choice==1){
-            base=new Database(new ListStrategy());
+        String[] options={"List"};
+        for (int i=0; i< options.length; i++){
+            System.out.println(options[i]);
         }
     }
 
--- src/classes/base/DbFactory.java
+++ src/classes/base/DbFactory.java
@@ -9,6 +9,6 @@ public class DbFactory {
             default:
                 return null;
         }
-        
+
     }
 }
